55ea15b20706bd08a1d52ad4cf03413eb9d31178
bantita24 <47713@kalasinpit.ac.th>
2025-02-17 15:47:20 +0700

    Update doubleSymbols.c

--- doubleSymbols.c
+++ doubleSymbols.c
@@ -4,11 +4,11 @@ int main() {
     int n; 
     scanf("%d", &n);
      if (n % 2 != 0) {
-            printf("Wrong input");
+         printf("Wrong input");
       } else {
-         for (int = 0; i < n; i+=2) {
+         for (int i = 0; i < n; i += 2) {
               printf("*+");
          }
       }
       return 0;
-      }
+    }
