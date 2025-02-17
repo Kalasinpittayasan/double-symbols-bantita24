3b1b2e8e63b32ad5ca8285d841bdd5fecdf96a7f
bantita24 <47713@kalasinpit.ac.th>
2025-02-17 15:45:31 +0700

    Update doubleSymbols.c

--- doubleSymbols.c
+++ doubleSymbols.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int n, 
+    int n; 
     scanf("%d", &n);
      if (n % 2 != 0) {
             printf("Wrong input");
       } else {
-      for (int = 0; i < n; i+=2) {
-          printf("*+");
-        }
+         for (int = 0; i < n; i+=2) {
+              printf("*+");
+         }
       }
       return 0;
       }
